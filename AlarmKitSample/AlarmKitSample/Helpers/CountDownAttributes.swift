b1ef7452d5e24bcc17af23831521381e69e2bdb4
akidon0000 <akihiro.matsuyama@sansan.com>
2025-06-22 13:37:45 +0900

    スケジュールアラート機能を実装

---
+++ AlarmKitSample/AlarmKitSample/Helpers/CountDownAttributes.swift
@@ -0,0 +1,14 @@
+//
+//  CountDownAttributes.swift
+//  AlarmKitSample
+//
+//  Created by Akihiro Matsuyama on 2025/06/22.
+//
+
+import SwiftUI
+import AlarmKit
+
+nonisolated
+struct CountDownAttributes: AlarmMetadata, Hashable, Sendable {
+    
+}
